fd45d43c8913bcd0310b2748d07e9a9d3ac325a5
Margaret Egner <hellogoodbyemae@gmail.com>
2020-07-13 16:11:13 -0400

    app created based off starwars and hotrest act

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name: "Mel",
+        photo: "",
+        scores: ["1", "5", "2", "1", "4", "4", "1", "5", "3", "1"]
+    },
+    {
+        name: "Ken",
+        photo: "",
+        scores: ["4", "5", "3", "5", "4", "2", "1", "3", "3", "1"]
+    },
+    {
+        name: "Jessica",
+        photo: "",
+        scores: ["2", "3", "5", "1", "3", "1", "2", "5", "4", "1"]
+    }
+    ];
+    
+    module.exports = friends;
