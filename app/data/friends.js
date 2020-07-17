7f12bbfac53e6293c97880cdfa534c18df3a434b
Margaret Egner <hellogoodbyemae@gmail.com>
2020-07-16 20:20:26 -0400

    fixed question class names for best match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,26 @@
 var friends = [
     {
-        name: "Mel",
+        name: "Woody",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTsTN1lNMuHqDmtH48TeKcRD3aF2WZ-g8-7xw&usqp=CAU",
         scores: [1, 5, 2, 1, 4, 4, 1, 5, 3, 1]
     },
     {
-        name: "Ken",
+        name: "Jerry",
         photo: "https://www.eironons.com/assets/images/tomandjerryholdingcheesecharactert-shirtiron-ontransferdecalctj4-1.jpg",
         scores: [4, 5, 3, 5, 4, 2, 1, 3, 3, 1]
     },
     {
-        name: "Jessica",
+        name: "Garfield",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRrl8_GCXQ4Gw9LQmKqOVd-krtVJc5zrSTSMQ&usqp=CAU",
         scores: [2, 3, 5, 1, 3, 1, 2, 5, 4, 1]
     },
     {
-        name: "Liz",
+        name: "Tweety",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwXev4F6wTXcDDVR0hJ0xaqQVGRxFTgKAnjw&usqp=CAU",
         scores: [1, 1, 3, 2, 4, 5, 5, 3, 1, 3]
     },
     {
-        name: "Jim",
+        name: "Dale",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRQpfgiG2YawdSkEY75pA-YNiIyEu98uLpJlw&usqp=CAU",
         scores: [3, 3, 1, 2, 5, 2, 1, 1, 4, 3]
     }
